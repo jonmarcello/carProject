f6a9eb52c7f4390ff325807575849fab6996f48c
albert <albert@10.250.10.247>
2017-02-21 16:53:21 -0500

    finalupload

--- src/carproject/Car.java
+++ src/carproject/Car.java
@@ -5,6 +5,7 @@
  */
 package carproject;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 
@@ -14,27 +15,25 @@ import java.util.ArrayList;
  * @author albert
  */
 public class Car {
-    
-    private String model;
+    String carManufacturer[] = {"honda","gm","volkswagon", "audi"};
+    private String model, make;
     private int year, milage;
     private double sellingPrice;
-    private String features[], make[];
+    private String features[];
     
-    public String[] getMake(){
-    ArrayList<String> carManufacturer = new ArrayList<String>();
-    carManufacturer.add("Ford");
-    carManufacturer.add("Honda");
-    carManufacturer.add("GM");
-    carManufacturer.add("Chevrolet");
-    carManufacturer.add("Volkswagon");
-    carManufacturer.add("Audi");
-    carManufacturer.add("Toyota");
-    carManufacturer.add("Acura");
-    carManufacturer.add("Dodge");
-
-        
+    public Car(String make,String model, int year, int milage, double sellingPrice, String[] features){
+     this.make = make;
+     this.model = model;
+     setYear(year);
+     this.milage = milage;
+     this.sellingPrice = sellingPrice;
+     this.features = features;
+    }
+    
+    public String getMake(){    
         return make;
     }
+    
     
     public String getModel(){
         return model;
@@ -44,24 +43,37 @@ public class Car {
         return features;
     }
     
-    public double getMilage(){
-        if(milage <0 && milage >1000000){
-            throw new IllegalArgumentException("Milage must be between 0, and 1 000 000 miles");
-        }else{
-        return milage;
-        }
-    }
-    
     public double getSellingPrice(){
         return sellingPrice;
     }
     
+    public int getMilage(){
+        return milage;
+    }
+    
     public int getYear(){
-      if(year<1900 && year>2018){
-          throw new IllegalArgumentException("year must be between 1900, and 2018");
-      }else{
         return year;
+    }
+    
+    public void setMilage(){
+        if(milage <0 && milage >1000000){
+            throw new IllegalArgumentException("Milage must be between 0, and 1 000 000 miles");
+        }else{
+        this.milage = milage;
+        }
+    }
+    public void setYear(int year){
+        
+      if(year>1900 && year<LocalDate.now().getYear() + 1){
+        this.year = year; 
+      }else{
+      throw new IllegalArgumentException("year must be between 1900, and 2018");
       }
+    
+    
+    
+    
+      
     }
     
     

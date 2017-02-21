523e7b26f636dbc62981fdbef4372c8c8a66fdd0
Nathan <Nathan@LAPTOP-0CP0IGBE>
2017-02-21 16:53:28 -0500

    New Controller

--- src/carproject/NewCarprojectController.java
+++ src/carproject/NewCarprojectController.java
@@ -11,7 +11,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 /**
@@ -33,7 +32,13 @@ public class NewCarprojectController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       Car newCar = new Car (makeComboBox.getValue(), 
+               modelTextField.getText(), 
+               Integer.parseInt(yearTextField.getText()), 
+               Integer.parseInt(milageTextField.getText()), 
+               Double.parseDouble(askingPriceTextField.getText()));
+       System.out.printf("The new vehicle is $s$n", newCar); 
+        
     }    
     
 }
